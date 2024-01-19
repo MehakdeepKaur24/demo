3d27e550e47568c96b929376b546873f17c75bf9
MehakdeepKaur24 <mehakdeep191103@gmail.com>
2024-01-19 13:07:14 -0500

    initial commit

---
+++ src/dayone/DayOne.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dayone;
+
+/**
+ *
+ * @author Mehakdeep Kaur
+ */
+public class DayOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+       System.out.println("Hwllo World...this is ,y first program on Net beans");
+       System.out.println("heyyyyyy");// TODO code application logic here
+    }
+    
+}
